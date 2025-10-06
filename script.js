397b60a757a287a98dfdd3e032682fe3a91a994f
jkdesign404 <04jackkelly@gmail.com>
2025-10-06 12:29:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const typed = new Typed('#typed-text', {
     strings: [
-      "Creative. Detail-oriented. Innovative. Dynamic. Precise. <a href='#portfolio' class='link'>Portfolio</a>. User-focused. Immersive. Adaptive. Functionality. Typographic. Modular. Accessible. <a href='#contact' class='link'>Contact</a>. Print-ready. Screen-optimized. Design. Engaging. <a href='#about' class='link'>About Me</a>. Modern. Impactful. Engaging. Purposeful. Cohesive. Dynamic. Visually compelling. Experimental. Striking. Balanced. Refined. Structured. Expressive. Polished."
+      "Hello. I am Jack Kelly. I am a designer based in Western North Carolina. I specialize in Branding and packaging. To see some of my work visit my <a href='#portfolio' class='link'>Portfolio</a> If your intrested in my work or just want to connect, <a href='#contact' class='link'>Contact</a> me. You can learn more <a href='#about' class='link'>About Me</a> and my skillsets aswell."
     ],
     typeSpeed: 1,
     backSpeed: 0,
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
     html: true
   });
 });
+
 
 
 
