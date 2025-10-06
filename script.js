ee036e2a49ced45e3250ef29f34ada7215cad0c8
jkdesign404 <04jackkelly@gmail.com>
2025-10-06 16:23:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     strings: [
       "Hello. I am Jack Kelly. I am a designer based in Western North Carolina. I specialize in Branding and packaging. To see some of my work visit my <a href='#portfolio' class='link'>Portfolio</a> If your intrested in my work or just want to connect, <a href='#contact' class='link'>Contact</a> me. You can learn more <a href='#about' class='link'>About Me</a> and my skillsets aswell."
     ],
-    typeSpeed: 1,
+    typeSpeed: 0.5,
     backSpeed: 0,
     showCursor: true,
     cursorChar: '|',
     html: true
   });
 });
+
 
 
 
