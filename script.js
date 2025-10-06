a9cc4968c1cf77fefbdefa885fca444f5e6413de
jkdesign404 <04jackkelly@gmail.com>
2025-10-06 10:55:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const typed = new Typed('#typed-text', {
     strings: [
-      "Creative. Detail-oriented. Innovative. Dynamic. Precise. Balanced. User-focused. Immersive. Adaptive. Functionality. Typographic. Modular. Accessible. Interactive. Print-ready. Screen-optimized. <a href='#portfolio' class='link'>Portfolio</a>. Design. Engaging. Clean. Modern. Impactful. Engaging. Purposeful. Cohesive. Dynamic. Visually compelling. Experimental. Striking. Balanced. Refined. Structured. Expressive. Polished. Strategic. Concept-driven. Bold. Sleek. Minimalistic. User-focused. Innovative. <a href='#about' class='link'>About Me</a>. Jack Kelly. Immersive. Adaptive. Harmonious. Detailed. Sharp. Aesthetic. High-quality. Professional. Timeless. Functional. Immersive. Memorable. Dedicated. Adaptable. Expressive. Ambitious. Observant. Analytical. Precise. Experimental. Dynamic. Strategic. Collaborative. Resourceful. Methodical. Intuitive. Bold. Meticulous. Intentional. Versatile. Conceptual. <a href='#contact' class='link'>Contact</a>."
+      "Creative. Detail-oriented. Innovative. Dynamic. Precise. <a href='#portfolio' class='link'>Portfolio</a>. User-focused. Immersive. Adaptive. Functionality. Typographic. Modular. Accessible. <a href='#contact' class='link'>Contact</a>. Print-ready. Screen-optimized. Design. Engaging. <a href='#about' class='link'>About Me</a>. Modern. Impactful. Engaging. Purposeful. Cohesive. Dynamic. Visually compelling. Experimental. Striking. Balanced. Refined. Structured. Expressive. Polished. Strategic. Concept-driven. Bold. Sleek. Minimalistic. User-focused. Innovative. Immersive. Adaptive. Harmonious. Detailed. Sharp. Aesthetic. High-quality. Professional. Timeless. Functional. Immersive. Memorable. Dedicated. Adaptable. Expressive. Ambitious. Observant. Analytical. Precise. Experimental. Dynamic. Strategic. Collaborative. Resourceful. Methodical. Intuitive. Bold. Meticulous. Intentional. Versatile. Conceptual."
     ],
     typeSpeed: 1,
     backSpeed: 0,
@@ -10,4 +10,5 @@ document.addEventListener("DOMContentLoaded", function() {
     html: true
   });
 });
+
 
