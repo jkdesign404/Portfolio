538d28b509b74d79febd5de5d587bf99d9639c9c
jkdesign404 <04jackkelly@gmail.com>
2025-10-06 11:31:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   const typed = new Typed('#typed-text', {
     strings: [
-      "Creative. Detail-oriented. Innovative. Dynamic. Precise. <a href='#portfolio' class='link'>Portfolio</a>. User-focused. Immersive. Adaptive. Functionality. Typographic. Modular. Accessible. <a href='#contact' class='link'>Contact</a>. Print-ready. Screen-optimized. Design. Engaging. <a href='#about' class='link'>About Me</a>. Modern. Impactful. Engaging. Purposeful. Cohesive. Dynamic. Visually compelling. Experimental. Striking. Balanced. Refined. Structured. Expressive. Polished. Strategic. Concept-driven. Bold. Sleek. Minimalistic. User-focused. Innovative. Immersive. Adaptive. Harmonious. Detailed. Sharp. Aesthetic. High-quality. Professional. Timeless. Functional. Immersive. Memorable. Dedicated. Adaptable. Expressive. Ambitious. Observant. Analytical. Precise. Experimental. Dynamic. Strategic. Collaborative. Resourceful. Methodical. Intuitive. Bold. Meticulous. Intentional. Versatile. Conceptual."
+      "Creative. Detail-oriented. Innovative. Dynamic. Precise. <a href='#portfolio' class='link'>Portfolio</a>. User-focused. Immersive. Adaptive. Functionality. Typographic. Modular. Accessible. <a href='#contact' class='link'>Contact</a>. Print-ready. Screen-optimized. Design. Engaging. <a href='#about' class='link'>About Me</a>. Modern. Impactful. Engaging. Purposeful. Cohesive. Dynamic. Visually compelling. Experimental. Striking. Balanced. Refined. Structured. Expressive. Polished."
     ],
     typeSpeed: 1,
     backSpeed: 0,
@@ -10,5 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     html: true
   });
 });
+
 
 
